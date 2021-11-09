35a6526d4e9e183d35e467e2cbc15b35a136e536
shriprasad deshpande <shriprasaddeshpande@gmail.com>
2021-11-09 21:57:05 +0530

    Create connect_four.java

---
+++ connect_four.java
@@ -0,0 +1,147 @@
+import java.util.Scanner;
+
+public class connect_four{
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[][] grid = new char[6][7];
+		
+		//creating array 
+		for (int row = 0; row < grid.length; row++){
+			for (int col = 0; col < grid[0].length; col++){
+				grid[row][col] = ' ';
+			}
+		}
+		
+		int turn = 1;
+		char player = 'R';
+		boolean winner = false;		
+		
+		//play a turn
+		while (winner == false && turn <= 42){
+			boolean validPlay;
+			int play;
+			do {
+				display(grid);
+				
+				System.out.print("Player " + player + ", choose a column: ");
+				play = sc.nextInt();
+				
+				//validate play
+				validPlay = validate(play,grid);
+				
+			}while (validPlay == false);
+			
+			//drop the checker
+			for (int row = grid.length-1; row >= 0; row--){
+				if(grid[row][play] == ' '){
+					grid[row][play] = player;
+					break;
+				}
+			}
+			
+			//determine if there is a winner
+			winner = isWinner(player,grid);
+			
+			//switch players
+			if (player == 'R'){
+				player = 'Y';
+			}else{
+				player = 'R';
+			}
+			
+			turn++;			
+		}
+		display(grid);
+		
+		if (winner){
+			if (player=='R'){
+				System.out.println("Yellow won");
+			}else{
+				System.out.println("Red won");
+			}
+		}else{
+			System.out.println(" game tie");
+		}
+		
+	}
+	
+	public static void display(char[][] grid){
+		
+		System.out.println("---------------");
+		for (int row = 0; row < grid.length; row++){
+			System.out.print("|");
+			for (int col = 0; col < grid[0].length; col++){
+				System.out.print(grid[row][col]);
+				System.out.print("|");
+			}
+			System.out.println();
+			System.out.println("---------------");
+		}
+		
+		System.out.println();
+	}
+	
+	public static boolean validate(int column, char[][] grid){
+		//valid column or not
+		if (column < 0 || column > grid[0].length){
+			return false;
+		}
+		
+		//full column or not
+		if (grid[0][column] != ' '){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isWinner(char player, char[][] grid){
+		//checking for 4 across blocks
+		for(int row = 0; row<grid.length; row++){
+			for (int col = 0;col < grid[0].length - 3;col++){
+				if (grid[row][col] == player   && 
+					grid[row][col+1] == player &&
+					grid[row][col+2] == player &&
+					grid[row][col+3] == player){
+					return true;
+				}
+			}			
+		}
+		//checking for 4 up and down blocks
+		for(int row = 0; row < grid.length - 3; row++){
+			for(int col = 0; col < grid[0].length; col++){
+				if (grid[row][col] == player   && 
+					grid[row+1][col] == player &&
+					grid[row+2][col] == player &&
+					grid[row+3][col] == player){
+					return true;
+				}
+			}
+		}
+		//checking for upward diagonal blocks
+		for(int row = 3; row < grid.length; row++){
+			for(int col = 0; col < grid[0].length - 3; col++){
+				if (grid[row][col] == player   && 
+					grid[row-1][col+1] == player &&
+					grid[row-2][col+2] == player &&
+					grid[row-3][col+3] == player){
+					return true;
+				}
+			}
+		}
+		//checking for downward diagonal blocks
+		for(int row = 0; row < grid.length - 3; row++){
+			for(int col = 0; col < grid[0].length - 3; col++){
+				if (grid[row][col] == player   && 
+					grid[row+1][col+1] == player &&
+					grid[row+2][col+2] == player &&
+					grid[row+3][col+3] == player){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
